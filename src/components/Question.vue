b7d94fed6a9da8b46cecc692fd436b84e5d90776
Konstantinos Mazgaltzidis <kamazgal@csd.auth.gr>
2023-10-08 09:51:45 +0300

    Init

---
+++ src/components/Question.vue
@@ -0,0 +1,132 @@
+<template >
+  <div class="container">
+    <div class="card mt-2">
+      <div class="card-body shadow-lg">
+        <div class="d-flex justify-content-between">
+          <p class="text-start fw-light">Film</p>
+          <div class="d-flex fs-5">
+            <p>1500</p>
+
+          </div>
+
+          <div class="d-flex fs-4 ">
+            ⌛<p class="text-end fw-light">16</p>
+          </div>
+        </div>
+        <p class="card-text fs-4 prevent-select" v-html="question"></p>
+      </div>
+    </div>
+
+    <div class="quiz-grid mt-4 gap-3">
+      <button
+          class="p-3 btn btn-outline-dark fs-3 shadow"
+          v-for="(choice, index) in choices"
+          :key="index"
+          v-html="choice"
+          @click.prevent="respond(choice)"
+          :class="{
+            'selected': selectedChoice === choice,
+            'blink_me btn-primary':  selectedChoice === choice && !showResult,
+           'disabled': responded,
+           // 'blink_me' : selectedChoice === choice &&  showResult,
+           'wrong_answer' :  showResult && selectedChoice === choice && choice !== correctChoice,
+           'correct_answer blink_me_faster' : showResult && choice === correctChoice
+          }"
+      ></button>
+    </div>
+
+
+<!--    <Scoreboard></Scoreboard>-->
+
+
+
+
+  </div>
+</template>
+
+<script>
+import Scoreboard from "./Scoreboard.vue";
+
+export default {
+  components: { Scoreboard },
+
+  data() {
+    return {
+      question: 'Which musical artist had a prominent role in the 2017 film &quot;Kingsman: The Golden Circle&quot;?',
+      choices: {
+        choice1: 'Elton John',
+        choice2: 'Lady Gaga',
+        choice3: 'Rihanna',
+        choice4: 'Justin Bieber',
+      },
+      selectedChoice: null,
+      correctChoice: 'Justin Bieber',
+      showResult: false,
+      isCorrect: false,
+      responded: false, // Track if the user has already responded
+      flashing: false
+    };
+  },
+  computed: {},
+  methods: {
+    respond(choice) {
+      if (!this.responded) {
+        this.selectedChoice = choice;
+        this.isCorrect = this.selectedChoice === this.correctChoice;
+        this.responded = true; // Mark that the user has responded
+
+        // setTimeout(() => this.showResult = true, 3000);
+        //
+        //this.showResult = true;
+
+        }
+      },
+    toggleDarkMode() {
+      const body = document.body;
+      body.classList.toggle('dark-mode');
+    }
+    }
+}
+</script>
+
+<style scoped>
+/* Custom CSS to make buttons expand to full width */
+.quiz-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: 1fr;
+  min-height: 15em;
+  background: white;
+}
+
+/* Style for the selected choice */
+.selected {
+  background-color: #007bff; /* Customize the background color as needed */
+  color: white;
+}
+.blink_me {
+  animation: blinker 1.5s infinite;
+}
+
+.blink_me_faster {
+  animation: blinker 0.8s linear infinite;
+}
+
+.correct_answer{
+  background-color: darkgreen; /* Customize the background color as needed */
+}
+.wrong_answer{
+  background-color: red; /* Customize the background color as needed */
+
+}
+
+
+@keyframes blinker {
+  50% {
+    opacity: 50;
+  }
+}
+
+
+
+</style>
---
+++ src/components/Scoreboard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "Scoreboard",
+}
+</script>
+
+<template>
+  <div class="container mt-5">
+    <table class="table table-striped table-sm fw-light shadow-lg">
+      <tbody>
+      <tr v-for="index in 2" :key="index">
+        <td>Player {{ index }}</td>
+        <td>{{ 1 }}</td>
+        <td>{{ 3 }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
